959736baf44e51393c5d6a8537b5dc83a778f122
Alberto Rodríguez R <albrodriguezr@unal.edu.co>
2020-07-09 09:12:59 -0500

    frec redes sociales

--- RCuentas2019.R
+++ RCuentas2019.R
@@ -20,7 +20,10 @@ Fuentes <- read.delim("Data/Preguntas.txt", header = TRUE)
 Fuentes$Medio <- as.character(Fuentes$Medio)
 Fuentes$Pregunta <- as.character(Fuentes$Pregunta)
 
-Fuentes %>% group_by(Medio) %>% count()
+# PREGUNTAS REDES SOCIALES - DIA AUDIENCIA
+
+Redes <- Fuentes %>% filter(Fuentes != 'Página')
+
 
 barplot(table(Fuentes$Medio))
 
@@ -89,10 +92,59 @@ cc <- docs[["content"]][["content"]]
 write.csv(cc, file = "Data/General.csv",  row.names = FALSE)
 
 
+
+# ANALISIS PREGUNTAS REDES SOCIALES
+
 # Facebook
 # Twitter
 # Youtube
 
+
+
+
+
+
+# Encoding(Fuentes)  <- "UTF-8"
+
+docs <- Corpus(VectorSource(Redes$Pregunta))
+
+
+
+# Convertir letras a minúsculas
+docs <- tm_map(docs, content_transformer(tolower))
+
+# Eliminar artículos, preposiciones, etc... del español
+docs <- tm_map(docs, removeWords, stopwords("spanish"))
+
+# Remover signos de puntuación
+docs <- tm_map(docs, removePunctuation)
+
+# Eliminar caracteres especiales
+
+removeSpecialChars <- function(x) str_replace_all(x, "[[:punct:]]", " ")
+docs <- tm_map(docs, removeSpecialChars)
+
+# Eliminar números
+docs <- tm_map(docs, removeNumbers)
+
+# Remover palabras de manera manual
+
+docs <- tm_map(docs, removeWords, c("pej", "i", "ii", "iii", "iv", "v", "vi", 
+                                    "unc", "universidad", "nacional", "colombia",
+                                    "cuáles", "cuales", "cuándo", "cómo", "cuál", "hacer",
+                                    "saber", "gracias", "unal", "cuenta", "quisiera", "van",
+                                    "launalcuenta", "tan", "pregunta", "gustaría", "así", "respecto",
+                                    "cada", "puede", "puedo", "quiero", "hace", "parte", "realizar",
+                                    "manera")) 
+
+# Matriz
+
+docs <- TermDocumentMatrix(docs)
+m <- as.matrix(docs)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+
+write.csv(d, file = "Data/FrecRedes.csv",  row.names = FALSE)
 
 
 
